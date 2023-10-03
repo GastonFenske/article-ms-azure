2d6155d74f92803bae2471207365ce69fc0eae0e
GastonFenske <thesacha2000f@gmail.com>
2023-10-03 17:40:58 -0300

    add credentials

---
+++ generate_credentials.sh
@@ -0,0 +1,9 @@
+
+GROUP_NAME=umGroupResource
+APP_NAME=my-flask-app-articles
+SUSCRIPTION_ID=9682b5b1-86a1-4971-982c-b5ae60c23814
+
+# echo $ACR_REGISTRY_ID
+
+az ad sp create-for-rbac --name $APP_NAME --role contributor \
+                        --scopes /subscriptions/$SUSCRIPTION_ID/resourceGroups/$GROUP_NAME \
