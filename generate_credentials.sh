e699fd359323bf400670564d7e9a5878b0311a84
GastonFenske <thesacha2000f@gmail.com>
2023-10-03 17:50:30 -0300

    adding credentials

--- generate_credentials.sh
+++ generate_credentials.sh
@@ -7,3 +7,4 @@ SUSCRIPTION_ID=9682b5b1-86a1-4971-982c-b5ae60c23814
 
 az ad sp create-for-rbac --name $APP_NAME --role contributor \
                         --scopes /subscriptions/$SUSCRIPTION_ID/resourceGroups/$GROUP_NAME \
+                        --sdk-auth
