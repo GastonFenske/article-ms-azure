2af92d8daef54515f2fd65ac1dac401f46b3a7e6
GastonFenske <thesacha2000f@gmail.com>
2023-10-10 20:05:14 -0300

    deplot this now v2

--- username_password.sh
+++ username_password.sh
@@ -3,7 +3,7 @@ ACR_NAME=stocklister
 ACR_REGISTRY_ID=$(az acr show --name $ACR_NAME --query "id" --output tsv)
 
 USER_NAME=$(az ad sp list --display-name $SERVICE_PRINCIPAL_NAME --query "[].appId" --output tsv)
-PASSWORD=$(az ad sp create-for-rbac --name $SERVICE_PRINCIPAL_NAME --scopes $ACR_REGISTRY_ID --role acrpull --query password --output tsv)
+# PASSWORD=$(az ad sp create-for-rbac --name $SERVICE_PRINCIPAL_NAME --scopes $ACR_REGISTRY_ID --role acrpull --query password --output tsv)
 
 echo $USER_NAME
-echo $PASSWORD
+# echo $PASSWORD
