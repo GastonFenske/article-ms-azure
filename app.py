ab6c34feb66776af0c90aa403c25de397a48d99f
GastonFenske <thesacha2000f@gmail.com>
2023-10-17 18:21:27 -0300

    Opentelemetry deploy

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ import os
 from main import create_app, db
 from main.models import ArticleModel, CategoryModel
 
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
+
+
 # import logging
 # logging.basicConfig()
 # logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
@@ -11,12 +22,32 @@ app = create_app()
 
 app.app_context().push()
 
+# Set global TracerProvider before instrumenting
+trace.set_tracer_provider(
+    TracerProvider(
+        resource=Resource.create({SERVICE_NAME: "article-ms-opentelemetry"})
+    )
+)
+
+# Enable tracing for sqlalchemy library
+SQLAlchemyInstrumentor().instrument()
+
+# Enable tracing for Flask library
+FlaskInstrumentor().instrument_app(app)
+
+# Enable tracing for requests library
+RequestsInstrumentor().instrument()
+
+trace_exporter = AzureMonitorTraceExporter(
+    connection_string="InstrumentationKey=63c8e473-95e8-40ee-822f-819240c88ef0;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/"
+)
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(trace_exporter)
+)
+
+
 @app.route('/healthcheck', methods=['GET'])
 def health_check():
-    # Verificar aquí la salud de tu aplicación
-    # Puedes agregar lógica adicional, como verificar conexiones a la base de datos u otros servicios
-
-    # Devolver código de estado 200 si la aplicación está saludable
     return 'App working correctly', 200
 
 if __name__ == '__main__':
